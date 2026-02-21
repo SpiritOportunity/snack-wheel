853e73495dd7637d3a82018b086b683ef75f1f1a
Aparna Pillai <pillaiaparna@gmail.com>
2026-02-21 13:07:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ spinBtn.addEventListener("click", () => {
   const extra = Math.floor(Math.random() * 360);
   const spins = 3 + Math.floor(Math.random() * 3);
   rotation += spins * 360 + extra;
-  wheel.style.transform = rotate(${rotation}deg);
+  wheel.style.transform = `rotate(${rotation}deg)`; 
 });
